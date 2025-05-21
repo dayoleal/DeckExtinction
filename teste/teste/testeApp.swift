c5765ec1d7698f56efa0d4289075a71071a10e07
Lucas Zanini <alunomack@10-44-48-20.0200.mack.br.iosda.org>
2025-05-21 14:44:21 -0300

    criação do projeto

---
+++ teste/teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Aluno Mack on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
