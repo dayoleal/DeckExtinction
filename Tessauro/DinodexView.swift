d808f9892fbd1c318cad6b0c205a2ae5d6448b72
Dayô Araújo <dayoleal@MacBook-Pro-85.local>
2025-06-11 15:13:07 -0300

    fix: responsivity

--- Tessauro/DinodexView.swift
+++ Tessauro/DinodexView.swift
@@ -2,96 +2,119 @@ import SwiftUI
 
 struct DinodexView: View {
     var body: some View {
-        NavigationStack {
-            ZStack {
+        GeometryReader { geometry in
+            NavigationStack {
                 ZStack {
-                    Color.lightBackground
-                        .ignoresSafeArea()
-                    
-                    VStack {
-                        Text("PLATEOSSAURO")
-                            .font(.title)
-                            .padding(.top)
+                    ZStack {
+                        Color.lightBackground
+                            .ignoresSafeArea()
                         
-                        Spacer()
-                        ZStack {
-                            Rectangle()
-                                .fill(.brownBackground)
-                                .ignoresSafeArea()
-                                .frame(height: 480)
-                            VStack {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .fill(.brownSecondary)
-                                        .frame(width: 340, height: 360)
-                                        .padding(.top)
-                                    
-                                    VStack (spacing: 15) {
-                                        Text("TRIÁSSICO")
-                                            .font(.title3)
-                                            .fontWeight(.bold)
-                                        
-                                        VStack(alignment: .leading) {
-                                            HStack (spacing: 30) {
-                                                Indicator()
-                                                Indicator2()
-                                            }
-                                            HStack (spacing: 35){
-                                                Text("Comprimento: 8 m")
-                                                Text("Peso: 4 t")
-                                            }
-                                        }.fontWeight(.bold)
-                                        
-                                        Text("Você  sabia que o Plateossauro vivia em bandos? Uma evidência disso foram fósseis encontrados com dezenas de esqueletos, indicando mortes em massa causadas por enchentes ou lamaçais.")
-                                            .frame(width: 305)
-                                        
-                                        Text("Essa era a principal estratégia de defesa contra predadores carnívoros, que eram maiores e mais ágeis.")
-                                            .frame(width: 305)
-                                    }
-                                    .foregroundStyle(.yellowLetter)
-                                    
-                                    HStack {
-                                        Spacer()
-                                        ZStack {
-                                            Circle()
-                                                .frame(width: 45, height: 45)
-                                                .foregroundStyle(.greenBrackground)
-                                            Image(systemName: "leaf.fill")
-                                                .foregroundStyle(.greenLeaf)
-                                        }
-                                        .padding(.bottom, 320)
-                                    }
-                                }.padding()
-                                
-                                NavigationLink {
-                                    ContentView(winView: false)
-                                } label: {
+                        VStack {
+                            Text("PLATEOSSAURO")
+                                .font(.title)
+                                .padding(.top)
+                            
+                            Spacer()
+                            ZStack {
+                                Rectangle()
+                                    .fill(.brownBackground)
+                                    .ignoresSafeArea()
+                                    .frame(height: geometry.size.height * 0.68)
+                                VStack {
                                     ZStack {
-                                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                            .frame(width: 150, height: 50)
-                                        HStack {
-                                            Image(systemName: "gamecontroller")
-                                                .foregroundStyle(.black)
-                                                .bold()
-                                            Text("Jogar")
-                                                .font(.headline)
+                                        if geometry.size.height < 700 {
+                                            RoundedRectangle(cornerRadius: 25)
+                                                .fill(.brownSecondary)
+                                                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.45)
+                                                .padding(.top)
+                                        } else {
+                                            RoundedRectangle(cornerRadius: 25)
+                                                .fill(.brownSecondary)
+                                                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.5)
+                                                .padding(.top)
+                                        }
+                                        
+                                        VStack (spacing: 15) {
+                                            Text("TRIÁSSICO")
+                                                .font(.title3)
                                                 .fontWeight(.bold)
-                                                .foregroundColor(.black)
+                                            
+                                            VStack(alignment: .leading) {
+                                                HStack (spacing: 30) {
+                                                    Indicator()
+                                                    Indicator2()
+                                                }
+                                                HStack (spacing: 35){
+                                                    Text("Comprimento: 8 m")
+                                                    Text("Peso: 4 t")
+                                                }
+                                            }.fontWeight(.medium)
+                                            
+                                            if geometry.size.height < 700 {
+                                                Text("Você  sabia que o Plateossauro vivia em bandos? Uma evidência disso foram fósseis encontrados com dezenas de esqueletos, indicando mortes em massa causadas por enchentes ou lamaçais.")
+                                                    .frame(width: geometry.size.width * 0.84, height: geometry.size.height * 0.2)
+                                            } else {
+                                                Text("Você  sabia que o Plateossauro vivia em bandos? Uma evidência disso foram fósseis encontrados com dezenas de esqueletos, indicando mortes em massa causadas por enchentes ou lamaçais.\n\nEssa era a principal estratégia de defesa contra predadores carnívoros, que eram maiores e mais ágeis.")
+                                                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.26)
+                                            }
+                                        }
+                                        .foregroundStyle(.yellowLetter)
+                                        
+                                        if geometry.size.height < 700 {
+                                            HStack {
+                                                Spacer()
+                                                ZStack {
+                                                    Circle()
+                                                        .frame(width: 45, height: 45)
+                                                        .foregroundStyle(.greenBrackground)
+                                                    Image(systemName: "leaf.fill")
+                                                        .foregroundStyle(.greenLeaf)
+                                                }
+                                            }.padding(.bottom, geometry.size.height * 0.41)
+                                        } else {
+                                            HStack {
+                                                Spacer()
+                                                ZStack {
+                                                    Circle()
+                                                        .frame(width: 45, height: 45)
+                                                        .foregroundStyle(.greenBrackground)
+                                                    Image(systemName: "leaf.fill")
+                                                        .foregroundStyle(.greenLeaf)
+                                                }
+                                            }.padding(.bottom, geometry.size.height * 0.46)
                                         }
                                     }
+                                    
+                                    NavigationLink {
+                                        ContentView(winView: false)
+                                    } label: {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                                .frame(width: 150, height: 50)
+                                            HStack {
+                                                Image(systemName: "gamecontroller")
+                                                    .foregroundStyle(.black)
+                                                    .bold()
+                                                Text("Jogar")
+                                                    .font(.headline)
+                                                    .fontWeight(.bold)
+                                                    .foregroundColor(.black)
+                                            }
+                                        }
+                                    }.padding(.top)
                                 }
                             }
                         }
                     }
-                }
-                
-                ZStack {
-                    Image(.brownDinosaur)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 130, height: 260)
                     
-                }.padding(.bottom, 350)
+                    ZStack {
+                        Image(.brownDinosaur)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width * 0.85, height: geometry.size.height * 0.25)
+                        
+                    }.padding(.bottom, geometry.size.height * 0.5)
+                }
             }
         }
     }
--- Tessauro/HomeView.swift
+++ Tessauro/HomeView.swift
@@ -2,97 +2,119 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationStack {
-            ZStack {
+        GeometryReader { geometry in
+            NavigationStack {
                 ZStack {
-                    Color.lightBackground
-                        .ignoresSafeArea()
-                    
-                    VStack {
-                        HStack {
-                            NavigationLink {
-                                TutorialView()
-                            } label: {
-                                Label("", systemImage: "info.circle")
-                                    .font(.title)
-                            }
-                            
-                            
-                            Spacer()
-                            NavigationLink {
-                                DinodexView()
-                            } label: {
-                                ZStack {
-                                    Circle()
-                                        .frame(width: 45, height: 45)
-                                    Image("dino.egg")
+                        Color.lightBackground
+                            .ignoresSafeArea()
+                        
+                        VStack {
+                            HStack {
+                                NavigationLink {
+                                    TutorialView()
+                                } label: {
+                                    Label("", systemImage: "info.circle")
                                         .font(.title)
-                                        .foregroundStyle(.black)
-                                }
-                            }
-                        }
-                        .padding(30)
-                        
-                        Text("DeckExtinction")
-                            .font(Font.custom("fonte.ttf", size: 32))
-                        
-                        Spacer()
-                        ZStack {
-                            Rectangle()
-                                .fill(.brownBackground)
-                                .ignoresSafeArea()
-                                .frame(height: 360)
-                            VStack {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .fill(.brownSecondary)
-                                        .frame(width: 340, height: 200)
-                                    
-                                    Text("E se você tivesse nascido na Era Mesozoica como um dinossauro? Seria o predador supremo ou só mais uma vítima da implacável seleção natural?")
-                                        .frame(width: 280, height: 200)
-                                        .font(.headline)
-                                        .foregroundColor(.lightBackground)
-                                        .fontWeight(.bold)
-                                        .multilineTextAlignment(.center)
                                 }
                                 
+                                
+                                Spacer()
                                 NavigationLink {
-                                    ContentView(winView: false)
+                                    DinodexView()
                                 } label: {
                                     ZStack {
-                                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                                            .frame(width: 150, height: 50)
-                                        HStack {
-                                            Image(systemName: "gamecontroller")
-                                                .foregroundStyle(.black)
-                                                .bold()
-                                            Text("Jogar")
-                                                .font(.headline)
-                                                .fontWeight(.bold)
-                                                .foregroundColor(.black)
-                                        }
+                                        Circle()
+                                            .frame(width: 45, height: 45)
+                                        Image("dino.egg")
+                                            .font(.title)
+                                            .foregroundStyle(.black)
                                     }
-                                }.padding()
+                                }
+                            }
+                            .padding(30)
+                            
+                            Text("DeckExtinction")
+                                .font(Font.custom("ZCOOLKuaiLe-Regular.ttf", size: 32))
+                            
+                            Spacer()
+                            ZStack {
+                                Rectangle()
+                                    .fill(.brownBackground)
+                                    .ignoresSafeArea()
+                                    .frame(height: geometry.size.height * 0.44)
+                                VStack {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 25)
+                                            .fill(.brownSecondary)
+                                            .frame(width: geometry.size.width * 0.85, height: geometry.size.height * 0.25)
+                                        
+                                        Text("E se você tivesse nascido na Era Mesozoica como um dinossauro? Seria o predador supremo ou só mais uma vítima da implacável seleção natural?")
+                                            .frame(width: 280, height: 200)
+                                            .font(.headline)
+                                            .foregroundColor(.lightBackground)
+                                            .fontWeight(.bold)
+                                            .multilineTextAlignment(.center)
+                                    }
+                                    
+                                    if geometry.size.height < 700 {
+                                        NavigationLink {
+                                            ContentView(winView: false)
+                                        } label: {
+                                            ZStack {
+                                                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                                    .frame(width: 150, height: 50)
+                                                HStack {
+                                                    Image(systemName: "gamecontroller")
+                                                        .foregroundStyle(.black)
+                                                        .bold()
+                                                    Text("Jogar")
+                                                        .font(.headline)
+                                                        .fontWeight(.bold)
+                                                        .foregroundColor(.black)
+                                                }
+                                            }
+                                        }
+                                        .padding(.bottom)
+                                    } else {
+                                        NavigationLink {
+                                            ContentView(winView: false)
+                                        } label: {
+                                            ZStack {
+                                                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                                    .frame(width: 150, height: 50)
+                                                HStack {
+                                                    Image(systemName: "gamecontroller")
+                                                        .foregroundStyle(.black)
+                                                        .bold()
+                                                    Text("Jogar")
+                                                        .font(.headline)
+                                                        .fontWeight(.bold)
+                                                        .foregroundColor(.black)
+                                                }
+                                            }
+                                        }
+                                        .padding(.top)
+                                    }
+                                }
                             }
                         }
-                    }
-                }
-                
-                ZStack {
-                    Image(.brownDinosaur)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 130, height: 260)
                     
-                    Image(.greenDinasaur)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 130, height: 260)
-                        .scaleEffect(x: -1)
-                        .padding(.top, 15)
-                }.padding(.bottom, 120)
-            }
-        }.navigationBarBackButtonHidden(true)
+                    ZStack {
+                        Image(.brownDinosaur)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.35)
+                        
+                        Image(.greenDinasaur)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.35)
+                            .scaleEffect(x: -1)
+                            .padding(.top, 15)
+                    }.padding(.bottom, geometry.size.height * 0.096)
+                }
+            }.navigationBarBackButtonHidden(true)
+        }
     }
 }
 
